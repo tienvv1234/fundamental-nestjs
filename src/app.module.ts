da4129befc014effa959a3b2f6801032b0076f48
tienvv <tienvv1234@gmail.com>
2025-03-11 16:38:52 +0700

    ConfigModule.forRoot load env from .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CoffeesModule } from './coffees/coffees.module';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
+import { CoffeesModule } from './coffees/coffees.module';
 import { DatabaseModule } from './database/database.module';
+
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     CoffeesModule,
     TypeOrmModule.forRoot({
       type: 'postgres', // type of our database
-      host: 'localhost', // database host
-      port: 5432, // database host
-      username: 'postgres', // username
-      password: 'pass123', // user password
-      database: 'postgres', // name of our database
+      host: process.env.DATABASE_HOST, // database host
+      port: +(process.env.DATABASE_PORT || 5432), // database host
+      username: process.env.DATABASE_USER, // username
+      password: process.env.DATABASE_PASSWORD, // user password
+      database: process.env.DATABASE_NAME, // name of our database
       autoLoadEntities: true, // models will be loaded automatically (you don't have to explicitly specify the entities: [] array)
       synchronize: true, // your entities will be synced with the database(recommended: disable in production)
       logging: true,
